9221fdcd897621cd339bb481d1486a459c7c23f1
Jeff Kerpan <jkerpan@gmail.com>
2017-06-06 10:02:04 -0600

    Partially finished Ceaser Shift Cypher

--- caesarShift.js
+++ caesarShift.js
@@ -1,4 +1,26 @@
+'use strict';
+
+// const shift = {}
+
+function encode (str) {
+  var arr = str.split('').map(function(val) {
+    var code = val.codePointAt(0);
+    if (code >= 65 && code <= 90) {
+      if (code + 3 > 90) {
+        code -= 26;
+      } else {
+        code = code + 3;
+      }
+    } else if (code >= 97 && code <= 122) {
+      if (code + 3 > 122) {
+        code -= 26;
+      } else {
+        code = code +3;
+      }
+    }
+  });
+
+}
 
 
-
-module.exports = {}
+module.exports = {};
